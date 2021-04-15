fab417ad9e9b48a3cb8e36958455fb68ffb70dc0
Natalie <theliteshado@icloud.com>
2021-04-15 12:39:31 -0400

    html

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+
 // nav links
 document.querySelector('#home-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#home-content').classList.remove('hidden')
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+
 })
 
 document.querySelector('#signup-link').addEventListener('click', () => {
@@ -17,15 +21,30 @@ document.querySelector('#login-link').addEventListener('click', () => {
 document.querySelector('#logout-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#home-content').classList.remove('hidden')
+  document.querySelector('#signup-link').classList.remove('hidden')
+  document.querySelector('#login-link').classList.remove('hidden')
+  document.querySelector('#createbusiness-link').classList.add('hidden')
+  document.querySelector('#reviews-link').classList.add('hidden')
+  document.querySelector('#logout-link').classList.add('hidden')
 })
 
 document.querySelector('#business-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#profile-content').classList.remove('hidden')
+  document.querySelector('#allBusiness').classList.remove('hidden')
 })
 
+document.querySelector('#createbusiness-link').addEventListener('click', () => {
+  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
+  document.querySelector('#createbusiness').classList.remove('hidden')
+})
 
+document.querySelector('#reviews-link').addEventListener('click', () => {
+  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
+  document.querySelector('#reviews').classList.remove('hidden')
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
 
+})
 
 
 
@@ -33,7 +52,7 @@ document.querySelector('#business-link').addEventListener('click', () => {
 
 document.querySelector('#signup-form').addEventListener('submit', async (event) => {
   event.preventDefault()
- 
+
   const name = document.querySelector('#signup-name').value
   const email = document.querySelector('#signup-email').value
   const password = document.querySelector('#signup-password').value
@@ -45,11 +64,12 @@ document.querySelector('#signup-form').addEventListener('submit', async (event) 
           password: password
       })
       // console.log(response)
-
+      document.querySelector('#logout-link').classList.add('hidden')
+      document.querySelector('#createbusiness-link').classList.add('hidden')
+      document.querySelector('#reviews-link').classList.add('hidden')
 
       alert('Successfully signed up!')
   } catch (error) {
-      // console.log(error)
           alert('email is already used by someone')
   }
 
@@ -71,11 +91,56 @@ try {
 
   const userId = response.data.user.id
   localStorage.setItem('userId', userId)
-  // console.log(userId)
+  document.querySelector('#logout-link').classList.add('hidden')
+  document.querySelector('#createbusiness-link').classList.add('hidden')
+  document.querySelector('#reviews-link').classList.add('hidden')
 
   alert(`Welcome back ${response.data.user.name}! you are successfully logged in`)
 } catch (error) {
     console.log(error)
     alert('login failed')
 }
+})
+
+//home content
+
+document.querySelector('#home-link').addEventListener('click', async (event) => {
+  event.preventDefault()
+  alert(`W E L P!!`)
+
+  const userId = response.data.user.id
+  localStorage.getItem('userId', userId)
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+})
+
+//business content
+
+document.querySelector('#business-link').addEventListener('click', async (event) => {
+  event.preventDefault()
+
+  const userId = response.data.user.id
+  localStorage.getItem('userId', userId)
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+})
+
+//create business
+document.querySelector('#createbusiness-link').addEventListener('click', async (event) => {
+  event.preventDefault()
+
+  const userId = response.data.user.id
+  localStorage.getItem('userId', userId)
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+})
+
+//reviews
+document.querySelector('#reviews-link').addEventListener('click', async (event) => {
+  event.preventDefault()
+
+  const userId = response.data.user.id
+  localStorage.getItem('userId', userId)
+  // document.querySelector('#signup-link').classList.add('hidden')
+  // document.querySelector('#login-link').classList.add('hidden')
 })
